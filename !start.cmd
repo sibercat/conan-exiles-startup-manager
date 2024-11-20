cc6d5eb9afcf9f8d8f35b730c6ef4b521adbb092
Sibercat <planetdima@gmail.com>
2024-11-20 13:18:55 -0500

    Update !start.cmd

--- !start.cmd
+++ !start.cmd
@@ -1,3 +1,6 @@
 @echo off
-powershell Start-Process python -ArgumentList "server_monitor.py" -Verb RunAs
-pause
+title Initial start window
+cd /d "%~dp0"
+echo You can close this window...
+powershell Start-Process python -ArgumentList "server_monitor.py" -Verb RunAs -Wait
+pause >nul
